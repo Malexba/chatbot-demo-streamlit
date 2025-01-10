80fbfab760f59f58a09f3254080883d53cef8600
Malexba <80394779+Malexba@users.noreply.github.com>
2025-01-10 11:10:40 +0000

    intro

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,64 +1,32 @@
-import streamlit as st
-from huggingface_hub import login, InferenceApi
 import os
+from langchain_huggingface import HuggingFaceEndpoint
+import streamlit as st
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 ## Initialize the Llama API
 model_id = "meta-llama/Llama-3.1-8B"
 token = os.getenv("LLAMA_TOKEN")  # Use your secret token
 st.title(token)
 
-'''
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+model_id="mistralai/Mistral-7B-Instruct-v0.3"
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+def get_llm_hf_inference(model_id=model_id, max_new_tokens=128, temperature=0.1):
+    """
+    Returns a language model for HuggingFace inference.
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    Parameters:
+    - model_id (str): The ID of the HuggingFace model repository.
+    - max_new_tokens (int): The maximum number of new tokens to generate.
+    - temperature (float): The temperature for sampling from the model.
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-'''
+    Returns:
+    - llm (HuggingFaceEndpoint): The language model for HuggingFace inference.
+    """
+    llm = HuggingFaceEndpoint(
+        repo_id=model_id,
+        max_new_tokens=max_new_tokens,
+        temperature=temperature,
+        token = os.getenv("HF_TOKEN")
+    )
+    return llm
